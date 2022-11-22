44673bcb902fcb92ae448f9326d2977b09c79ea2
angelov-g <georgi.angelov.2508@gmail.com>
2022-11-22 15:04:37 -0500

    added list-accounts functionality

--- service/routes.py
+++ service/routes.py
@@ -62,7 +62,17 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
-
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """
+    List all Accounts
+    This endpoint will list all Accounts
+    """
+    app.logger.info("Request to list Accounts")
+    accounts = Account.all()
+    account_list = [account.serialize() for account in accounts]
+    app.logger.info("Returning [%s] accounts", len(account_list))
+    return jsonify(account_list), status.HTTP_200_OK
 
 ######################################################################
 # READ AN ACCOUNT
