f96c1f5802e43bafb380294a3812009a4e730575
angelov-g <georgi.angelov.2508@gmail.com>
2022-11-22 15:01:23 -0500

    added delete-account functionality

--- service/routes.py
+++ service/routes.py
@@ -105,7 +105,17 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
-
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    """
+    Delete an Account
+    This endpoint will delete an Account based on the account_id that is requested
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+    return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
